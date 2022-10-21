d774c2ce22cabed40ec3bfa81680bf43ce419185
Max.-F.Helm <m7t24zklr@relay.firefox.com>
2022-10-21 21:45:06 +0200

    add some exception types

---
+++ src/exceptions.ts
@@ -0,0 +1,13 @@
+// some Java-like exceptions and more
+
+export class IllegalArgumentException extends Error {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
+
+export class FileContentsException extends Error {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
