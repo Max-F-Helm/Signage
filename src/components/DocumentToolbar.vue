9d3a7897b9221c96ab2ad258244f7fb5e41a2c13
Peter Nerlich <peter.nerlich+dev@googlemail.com>
2022-10-22 17:14:17 +0200

    modeled HomeView, added DocumentToolbar, mock DocumentHistory

---
+++ src/components/DocumentToolbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <Toolbar>
+      <template #start>
+        <MyButton label="Vote für letzten Vorschlag" icon="pi pi-check" @click="loadLatestDocument" class="p-button-success" />
+      </template>
+      <template #end>
+        <MyButton label="Patchset importieren" icon="pi pi-upload" @click="loadLatestDocument" />
+      </template>
+    </Toolbar>
+  </main>
+</template>
+
+<script>
+import MyButton from 'primevue/button';
+import Toolbar from 'primevue/toolbar';
+export default {
+  name: "StartPage",
+  components:{
+    MyButton,
+    Toolbar
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+  Toolbar {
+    border-radius: 0;
+    border: none;
+  }
+</style>
