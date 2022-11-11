d7c957b68dfbf577b86f3576728c84e599a1d4f4
Max.-F.Helm <m7t24zklr@relay.firefox.com>
2022-11-11 14:47:49 +0100

    add support for audio- and video-preview in Addendum

--- src/ui/file_spec/components/Addendum.vue
+++ src/ui/file_spec/components/Addendum.vue
@@ -34,8 +34,14 @@
           <div v-else-if="isImage" class="flex justify-content-center">
             <Image :src="asBlob()" alt="invalid image" :preview="true" imageClass="max-h-24rem"></Image>
           </div>
-          <div v-else-if="isPdf">
+          <div v-else-if="isPdf" class="flex justify-content-center">
             <iframe :src="asBlob()" class="pdfViewer"></iframe>
+          </div>
+          <div v-else-if="isAudio" class="flex justify-content-center">
+            <audio :src="asBlob()" controls class="w-full mx-2"></audio>
+          </div>
+          <div v-else-if="isVideo" class="flex justify-content-center">
+            <video :src="asBlob()" controls class="max-h-24rem"></video>
           </div>
           <div v-else class="flex justify-content-center font-bold text-xl">
             No preview available
@@ -52,7 +58,7 @@
   import PButton from "primevue/button";
   import Image from "primevue/image";
   import type {PropType} from "vue";
-  import {computed} from "vue";
+  import {computed, onUnmounted} from "vue";
   import type Addendum from "@/processing/model/Addendum";
   import {download, formatDateTime} from "@/ui/utils/utils";
   import {Buffer} from "buffer";
@@ -87,6 +93,14 @@
     return mime.startsWith("application/pdf")
         || mime.startsWith("application/x-pdf");
   });
+  const isAudio = computed(() => {
+    return props.val.type.startsWith("audio/");
+  });
+  const isVideo = computed(() => {
+    return props.val.type.startsWith("video/");
+  });
+
+  let blobUrl: string | null = null;
 
   function onDownload() {
     //TODO infer file-extension from mime (if title has no extension)
@@ -97,10 +111,21 @@
     return Buffer.from(props.val.data).toString("utf-8");
   }
   function asBlob(): string {
-    const frame = props.val;
-    const base64 = Buffer.from(frame.data).toString("base64");
-    return `data:${frame.type};base64,${base64}`;
+    if(blobUrl !== null)
+      URL.revokeObjectURL(blobUrl);
+
+    const addendum = props.val;
+    const blob = new Blob([addendum.data], {
+      type: addendum.type
+    });
+    blobUrl = URL.createObjectURL(blob);
+    return blobUrl;
   }
+
+  onUnmounted(() => {
+    if(blobUrl !== null)
+      URL.revokeObjectURL(blobUrl);
+  });
 </script>
 
 <style lang="scss">
